12e98b2bcc4cba43e9e534cd6eb2e401933968da
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-08-15 20:46:00 +0500

    +

--- src/shared/components/carousel/Carousel.tsx
+++ src/shared/components/carousel/Carousel.tsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
-
-// import "swiper/css";
-// import "swiper/css/free-mode";
-// import "swiper/css/navigation";
-// import "swiper/css/thumbs";
-
 import { useMovie } from "../../../features/movies/service/useMovie";
 
 const Carousel = () => {
@@ -22,48 +16,53 @@ const Carousel = () => {
     .slice(0, 10);
 
   return (
-    <>
+    <div className="mx-auto max-w-[1200px] px-4">
+      {/* KATTA SLAYDER */}
       <Swiper
-        style={{
-          // @ts-ignore
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
+        spaceBetween={12}
+        navigation
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
         }}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
         modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
+        className="mySwiper2 h-[420px] md:h-[520px] rounded-2xl overflow-hidden shadow-2xl
+                   [--swiper-navigation-color:#fff] [--swiper-pagination-color:#fff]"
       >
         {movies.map((movie: any) => (
           <SwiperSlide key={movie.id}>
             <img
+              className="w-full h-full object-cover block"
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt={movie.title || movie.original_title}
+              loading="lazy"
             />
           </SwiperSlide>
         ))}
       </Swiper>
 
+      {/* THUMB SLIDER */}
       <Swiper
         onSwiper={setThumbsSwiper}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
+        spaceBetween={12}
+        slidesPerView={6}
+        freeMode
+        watchSlidesProgress
         modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper"
+        className="mySwiper mt-4 pb-2"
       >
         {movies.map((movie: any) => (
-          <SwiperSlide key={movie.id}>
+          <SwiperSlide key={movie.id} className="cursor-pointer">
             <img
+              className="h-[110px] md:h-[130px] w-full object-cover rounded-xl
+                         ring-1 ring-white/10 hover:ring-white/40 transition block"
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title || movie.original_title}
+              loading="lazy"
             />
           </SwiperSlide>
         ))}
       </Swiper>
-    </>
+    </div>
   );
 };
 
