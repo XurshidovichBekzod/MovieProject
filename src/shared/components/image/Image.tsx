afad7fdc356aa8ad2ff5ee3467331566ea602a91
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-08-18 21:51:58 +0500

    +

--- src/shared/components/image/Image.tsx
+++ src/shared/components/image/Image.tsx
@@ -1,22 +1,31 @@
-import React, { useState, type FC } from 'react'
+import { useState, type FC } from 'react'
 
- interface Props {
-     src: string
-     alt?: string
-     className?: string
-     height: number
- }
+interface Props {
+  src: string
+  alt?: string
+  className?: string
+  height: number
+}
 
- const Image:FC<Props> = ({src, alt="", className="", height}) => {
-     const [loading, setLoading] = useState(true)
-     return (
-         <div>
-             <img src={src} className={className} onLoad={() => setLoading(false)} alt={alt} />
-             {
-                 loading && <div style={{height: height+"px"}} className='w-full bg-[#303030]'></div>
-             }
-         </div>
-     )
- }
+const Image: FC<Props> = ({ src, alt = "", className = "", height }) => {
+  const [loading, setLoading] = useState(true)
 
- export default Image
+  return (
+    <div>
+      <img
+        src={src}
+        className={className}
+        onLoad={() => setLoading(false)}
+        alt={alt}
+      />
+      {loading && (
+        <div
+          style={{ height: height + "px" }}
+          className="w-full bg-[#303030]"
+        ></div>
+      )}
+    </div>
+  )
+}
+
+export default Image
